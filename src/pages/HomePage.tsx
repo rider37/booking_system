6f40a35e77d7deb3c650962af50d321ac457f27f
rider37 <kgw1704@gmail.com>
2025-09-18 01:10:34 +0900

    4

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ export const HomePage: React.FC = () => {
   return (
     <div className="home-hero">
       <div className="home-overlay">
-        <h1 className="home-title">2025 극단 봄 정기공연, Way Down</h1>
+        <h1 className="home-title">2025 극단 봄 정기공연<br />Way Down<br />좌석 예매</h1>
         <div className="home-actions">
           <Link to="/seats"><button className="btn primary lg">좌석 예매</button></Link>
           <Link to="/lookup"><button className="btn lg">예약 확인</button></Link>
